306df7a7b4b95723b887d64f4f0326d1c1314faf
Towfiqul Islam <towfiqu@gmail.com>
2020-01-17 12:21:17 +0600

    parcel setup

---
+++ src/js/modules/Test.js
@@ -0,0 +1,13 @@
+class Test {
+  constructor() {
+    this.events();
+  }
+
+  events() {
+    const headline = document.querySelector('.headline');
+
+    // headline.classList.add('active');
+  }
+}
+
+export default Test;
---
+++ src/js/scripts.js
@@ -0,0 +1,3 @@
+import Test from './modules/Test';
+
+const test = new Test();
